c221b0f8eae38be2fc4691f374ea4e3e626466c1
Jameel Haffejee <rc1140@runawaycoder.co.za>
2020-07-12 11:42:15 +0200

    Add csv dumping and 5min never ending loop

--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"encoding/binary"
+	"encoding/csv"
 	"fmt"
+	"log"
 	"os"
+	"strings"
 	"time"
 
 	"flag"
@@ -48,7 +51,8 @@ func updateCount(db *bolt.DB, count uint16, user, activeBucket string) {
 		b := tx.Bucket([]byte(fmt.Sprintf("%s.%s", "dailycount", activeBucket)))
 
 		err := b.Put([]byte(user), itob(count))
-		fmt.Printf("Saving data for %s %d\n", user, count)
+		//TODO : Replace format strings with propper debug logs
+		// fmt.Printf("Saving data for %s %d\n", user, count)
 		if err != nil {
 			return fmt.Errorf("Error updating key: %s", err)
 		}
@@ -91,13 +95,64 @@ func getLastIndexedTweeID(db *bolt.DB) int64 {
 	return lastIndexedTweet
 }
 
+func getDailyUserCountBuckets(db *bolt.DB) []string {
+	var buckets []string
+	db.View(func(tx *bolt.Tx) error {
+		// Assume bucket exists and has keys
+		c := tx.Cursor()
+
+		for k, _ := c.First(); k != nil; k, _ = c.Next() {
+			bn := string(k)
+			if strings.HasPrefix(bn, "dailycount") {
+				buckets = append(buckets, bn)
+			}
+		}
+
+		return nil
+	})
+	return buckets
+}
+
+func dumpBucketToCSV(db *bolt.DB, bucket string) {
+	var userStats [][]string
+	userStats = append(userStats, []string{"bucket_date", "screen_name", "daily_tweets"})
+	bucketDate := strings.Split(bucket, ".")[1]
+	db.View(func(tx *bolt.Tx) error {
+		// Assume bucket exists and has keys
+		b := tx.Bucket([]byte(bucket))
+		c := b.Cursor()
+
+		for k, v := c.First(); k != nil; k, v = c.Next() {
+			c := binary.BigEndian.Uint16(v)
+			userStats = append(userStats, []string{bucketDate, string(k), fmt.Sprintf("%d", c)})
+		}
+
+		return nil
+	})
+
+	w := csv.NewWriter(os.Stdout)
+	w.WriteAll(userStats) // calls Flush internally
+
+	if err := w.Error(); err != nil {
+		log.Fatalln("error writing csv:", err)
+	}
+}
+
+//dbExport is used to automate extracting the stats
+func dbExport(db *bolt.DB) {
+	bcks := getDailyUserCountBuckets(db)
+
+	for _, bck := range bcks {
+		dumpBucketToCSV(db, bck)
+	}
+
+}
+
 func main() {
 	db, err := bolt.Open("analytics.db", 0666, nil)
 	if err != nil {
 		fmt.Println(err)
 	}
-
-	activeBucket := time.Now().Format("01/02/06")
 
 	defer db.Close()
 
@@ -107,47 +162,62 @@ func main() {
 	consumerSecret := flags.String("consumer-secret", "", "Twitter Consumer Secret")
 	accessToken := flags.String("access-token", "", "Twitter Access Token")
 	accessSecret := flags.String("access-secret", "", "Twitter Access Secret")
+	dumpCSV := flags.Bool("csv", false, "Set to true to dump all buckets to csv")
 
 	flags.Parse(os.Args[1:])
 	flagutil.SetFlagsFromEnv(flags, "TWITTER")
+
+	if *dumpCSV {
+		dbExport(db)
+		return
+	}
 
 	config := oauth1.NewConfig(*consumerKey, *consumerSecret)
 	token := oauth1.NewToken(*accessToken, *accessSecret)
 	httpClient := config.Client(oauth1.NoContext, token)
 
-	m := loadExistingCounts(db, activeBucket)
-
 	client := twitter.NewClient(httpClient)
+
 	var lastIndexedTweet int64 = 0
 
-	lastIndexedTweet = getLastIndexedTweeID(db)
+	for {
+		activeBucket := time.Now().Format("01/02/06")
+		//We technically dont need to load the data for long running proceses , this just ensures a bit more consistency if the app ever crashes
+		m := loadExistingCounts(db, activeBucket)
+		lastIndexedTweet = getLastIndexedTweeID(db)
 
-	fmt.Printf("Last indexed tweet %d \n", lastIndexedTweet)
+		fmt.Printf("Last indexed tweet %d \n", lastIndexedTweet)
 
-	tweets, _, err := client.Timelines.HomeTimeline(&twitter.HomeTimelineParams{
-		Count:   200,
-		SinceID: lastIndexedTweet,
-	})
-	if err != nil {
-		fmt.Println(err)
-	}
+		tweets, _, err := client.Timelines.HomeTimeline(&twitter.HomeTimelineParams{
+			Count:   200,
+			SinceID: lastIndexedTweet,
+		})
+		if err != nil {
+			fmt.Println(err)
+		}
 
-	fmt.Printf("Found %d tweets since last run", len(tweets))
-
-	if tweets != nil {
-		for _, x := range tweets {
-			// fmt.Println(fmt.Sprintf("-> %s", x.User.ScreenName))
-			// fmt.Println(fmt.Sprintf("** %s", x.Text))
-			m[x.User.ScreenName]++
-			if x.ID > lastIndexedTweet {
-				lastIndexedTweet = x.ID
+		if len(tweets) > 0 {
+			fmt.Printf("Found %d tweets since last run", len(tweets))
+			if tweets != nil {
+				for _, x := range tweets {
+					m[x.User.ScreenName]++
+					if x.ID > lastIndexedTweet {
+						lastIndexedTweet = x.ID
+					}
+				}
 			}
 
+			for user, count := range m {
+				updateCount(db, count, user, activeBucket)
+			}
+
+			putLastIndexedTweet(db, lastIndexedTweet)
+		} else {
+			fmt.Println("No New tweets found exiting")
 		}
+
+		fmt.Println("Sleeping for 5 mins")
+		time.Sleep(5 * time.Minute)
 	}
 
-	putLastIndexedTweet(db, lastIndexedTweet)
-	for user, count := range m {
-		updateCount(db, count, user, activeBucket)
-	}
 }
